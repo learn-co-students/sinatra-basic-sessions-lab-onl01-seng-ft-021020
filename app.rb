301b5d32b08ab98157f71ad30fc9a2ecb72fe168
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-03-24 23:11:25 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,29 @@
+
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    configure do
+        enable :sessions
+        set :session_secret, "secret"
+    end
+
+    get '/' do
+        @session = session
+        erb :'index'
+    end
+
+    post '/checkout' do
+      # session[:item] = "item"
+      # @session = session
+        @item = params[:item]
+        session["item"] = @item
+        
+        erb :'checkout'
+    end
 end
+
+# get '/hey' do 
+#   session["name"] = "Victoria"
+#   @session = session
+# end
